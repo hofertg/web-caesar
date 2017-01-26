7de6507ec707ba0a0c63312304944f76bdf68bc8
Tim Hofer <hofertg@hotmail.com>
2017-01-26 14:39:17 -0600

    Added textarea and submit button

--- main.py
+++ main.py
@@ -23,7 +23,12 @@ class MainHandler(webapp2.RequestHandler):
         message = "Hello world!"
         encrypted_message = caesar.encrypt(message, 13)
 
-        self.response.write(encrypted_message)
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = '<input type = "submit" />'
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        content = form
+
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
